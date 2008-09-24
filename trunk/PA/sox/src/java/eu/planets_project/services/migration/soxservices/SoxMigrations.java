5a4a78bc9767b1b82461508e53e8084f229bbdb6
Peter Melms <peter.melms@uni-koeln.de>
2008-09-24 13:51:23 +0000

    - Modified SoxMigrations.java to use the ByteArrayHelper methods in IF_SP/common/.

--- trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
+++ trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
@@ -1,11 +1,8 @@
 package eu.planets_project.services.migration.soxservices;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +13,7 @@ import javax.mail.util.ByteArrayDataSource;
 
 import eu.planets_project.ifr.core.common.cli.ProcessRunner;
 import eu.planets_project.ifr.core.common.logging.PlanetsLogger;
+import eu.planets_project.ifr.core.common.services.ByteArrayHelper;
 
 /**
  * Common sox invocation class
@@ -100,55 +98,6 @@ public class SoxMigrations {
 	}
 	
 
-	// FIXME Refactor this into common.
-	private static byte[] getByteArrayFromFile(File file) {
-		// Create the byte array to hold the data
-		byte[] bytes = null;
-		try {
-			InputStream is = new FileInputStream(file);
-
-			// Get the size of the file
-			long length = file.length();
-
-			// You cannot create an array using a long type.
-			// It needs to be an int type.
-			// Before converting to an int type, check
-			// to ensure that file is not larger than Integer.MAX_VALUE.
-			if (length > Integer.MAX_VALUE) {
-				// throw new
-				// IllegalArgumentException("getBytesFromFile@JpgToTiffConverter::
-				// The file is too large (i.e. larger than 2 GB!");
-				System.out
-						.println("Datei ist zu gross (e.g. groesser als 2GB)!");
-			}
-
-			bytes = new byte[(int) length];
-
-			// Read in the bytes
-			int offset = 0;
-			int numRead = 0;
-			while (offset < bytes.length
-					&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-				offset += numRead;
-			}
-
-			// Ensure all the bytes have been read in
-			if (offset < bytes.length) {
-				throw new IOException("Could not completely read file "
-						+ file.getName());
-			}
-
-			// Close the input stream and return bytes
-			is.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return bytes;
-	}
-
 	public byte[] genericTransformAudioSrcToAudioDest(byte[] input,
 			String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
 	
@@ -188,7 +137,7 @@ public class SoxMigrations {
 		log.info("genericTransformAudioSrcToAudioDest end");
 		log.info(f.length());
 	
-		return getByteArrayFromFile(f);
+		return ByteArrayHelper.read(f);
 	}
 
 	public DataHandler genericTransformAudioSrcToAudioDestDH(DataHandler input,
@@ -231,7 +180,7 @@ public class SoxMigrations {
 		}
 		log.info("genericTransformAudioSrcToAudioDestDH end");
 		log.info(f.length());
-		return new DataHandler(new ByteArrayDataSource(getByteArrayFromFile(f),
+		return new DataHandler(new ByteArrayDataSource(ByteArrayHelper.read(f),
 				"application/octet-stream"));
 	}
 
