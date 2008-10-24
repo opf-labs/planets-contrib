2743fda6297b64003398c84f789760bceac1ee0a
Fabian Steeg <fsteeg@gmail.com>
2008-10-24 16:55:41 +0000

    Different small changes in services, tests, and setup; now all tests pass (see pserv/tests for instructions on running the tests).

--- trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
+++ trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
@@ -23,165 +23,171 @@ import eu.planets_project.services.utils.ProcessRunner;
  */
 public class SoxMigrations {
 
-	public final String SOX = "sox";
-	PlanetsLogger log = PlanetsLogger.getLogger(SoxMigrations.class);
+    public final String SOX = "sox";
+    PlanetsLogger log = PlanetsLogger.getLogger(SoxMigrations.class);
 
-	public static String SOX_HOME;
+    public static String SOX_HOME;
 
-	public SoxMigrations() {
-		Properties props = new Properties();
-		try {
-			props
-					.load(this
-							.getClass()
-							.getResourceAsStream(
-									"/eu/planets_project/services/migration/soxservices/sox.properties"));
-			SOX_HOME = props.getProperty("sox.bin.dir");
+    public SoxMigrations() {
+        Properties props = new Properties();
+        try {
+            props
+                    .load(this
+                            .getClass()
+                            .getResourceAsStream(
+                                    "/eu/planets_project/services/migration/soxservices/sox.properties"));
+            SOX_HOME = props.getProperty("sox.bin.dir");
 
-		} catch (IOException e) {
-			// Use a default for now.
-			SOX_HOME = "/usr/bin/";
-		}
-		log.info("Pointed to sox in: " + SOX_HOME);
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Use a default for now.
+            SOX_HOME = "/usr/bin/";
+        }
+        log.info("Pointed to sox in: " + SOX_HOME);
+    }
 
-	public byte[] transformMp3ToOgg(byte[] input) {
-		log.info("transformMp3ToOgg begin ");
-		return genericTransformAudioSrcToAudioDest(input, ".mp3", ".ogg", null);
-	}
+    public byte[] transformMp3ToOgg(byte[] input) {
+        log.info("transformMp3ToOgg begin ");
+        return genericTransformAudioSrcToAudioDest(input, ".mp3", ".ogg", null);
+    }
 
-	public byte[] transformMp3ToWav(byte[] input) {
-		log.info("transformMp3ToWav begin ");
-		return genericTransformAudioSrcToAudioDest(input, ".mp3", ".wav", null);
-	}
+    public byte[] transformMp3ToWav(byte[] input) {
+        log.info("transformMp3ToWav begin ");
+        return genericTransformAudioSrcToAudioDest(input, ".mp3", ".wav", null);
+    }
 
-	public byte[] transformWavToOgg(byte[] input) {
-		log.info("transformWavToOgg begin ");
-		return genericTransformAudioSrcToAudioDest(input, ".wav", ".ogg", null);
-	}
+    public byte[] transformWavToOgg(byte[] input) {
+        log.info("transformWavToOgg begin ");
+        return genericTransformAudioSrcToAudioDest(input, ".wav", ".ogg", null);
+    }
 
-	public byte[] transformWavToFlac(byte[] input) {
-		log.info("transformWavToFlac begin ");
-		return genericTransformAudioSrcToAudioDest(input, ".wav", ".flac", null);
-	}
+    public byte[] transformWavToFlac(byte[] input) {
+        log.info("transformWavToFlac begin ");
+        return genericTransformAudioSrcToAudioDest(input, ".wav", ".flac", null);
+    }
 
-	public byte[] transformMp3ToFlac(byte[] input) {
-		log.info("transformMp3ToFlac begin ");
-		return genericTransformAudioSrcToAudioDest(input, ".mp3", ".flac", null);
-	}
-	
-	
-	
-	public DataHandler transformMp3ToOggDH(DataHandler input) {
-		log.info("transformMp3ToOggDH begin ");
-		return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".ogg", null);
-	}
+    public byte[] transformMp3ToFlac(byte[] input) {
+        log.info("transformMp3ToFlac begin ");
+        return genericTransformAudioSrcToAudioDest(input, ".mp3", ".flac", null);
+    }
 
-	public  DataHandler transformMp3ToWavDH( DataHandler input) {
-		log.info("transformMp3ToWavDH begin ");
-		return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".wav", null);
-	}
+    public DataHandler transformMp3ToOggDH(DataHandler input) {
+        log.info("transformMp3ToOggDH begin ");
+        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".ogg",
+                null);
+    }
 
-	public  DataHandler transformWavToOggDH( DataHandler input) {
-		log.info("transformWavToOggDH begin ");
-		return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".ogg", null);
-	}
+    public DataHandler transformMp3ToWavDH(DataHandler input) {
+        log.info("transformMp3ToWavDH begin ");
+        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".wav",
+                null);
+    }
 
-	public  DataHandler transformWavToFlacDH( DataHandler input) {
-		log.info("transformWavToFlacDH begin ");
-		return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".flac", null);
-	}
+    public DataHandler transformWavToOggDH(DataHandler input) {
+        log.info("transformWavToOggDH begin ");
+        return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".ogg",
+                null);
+    }
 
-	public  DataHandler transformMp3ToFlacDH( DataHandler input) {
-		log.info("transformMp3ToFlacDH begin ");
-		return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".flac", null);
-	}
-	
+    public DataHandler transformWavToFlacDH(DataHandler input) {
+        log.info("transformWavToFlacDH begin ");
+        return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".flac",
+                null);
+    }
 
-	public byte[] genericTransformAudioSrcToAudioDest(byte[] input,
-			String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
-	
-		if (!srcSuffix.startsWith("."))
-			srcSuffix = "." + srcSuffix;
-		if (!destSuffix.startsWith("."))
-			destSuffix = "." + destSuffix;
-		log.info("genericTransformAudioSrcToAudioDest begin: Converting from "
-				+ srcSuffix + " to " + destSuffix);
-		File f = FileUtils.tempFile("tempout", destSuffix);
-	
-		File inputFile = FileUtils.tempFile(input, "tempin.audio", srcSuffix);
-		if (!f.canRead() || !inputFile.exists() || !f.exists() || !f.canWrite()
-				|| !inputFile.canRead() || !inputFile.canWrite()) {
-			throw new IllegalStateException("Can't read from or write to: "
-					+ f.getAbsolutePath());
-		}
-	
-		try {
-			List<String> commands = Arrays.asList(SOX, inputFile
-					.getAbsolutePath(), f.getAbsolutePath());
-			commands.addAll(soxCliParams);
-			File home = new File(SOX_HOME);
-	
-			ProcessRunner pr = new ProcessRunner(commands);
-			pr.setStartingDir(home);
-			log.info("Executing: " + commands);
-			pr.run();
-	
-			log.info("SOX call output: " + pr.getProcessOutputAsString());
-			log.error("SOX call error: " + pr.getProcessErrorAsString());
-			log.debug("Executing: " + commands + " finished.");
-	
-		} catch (Exception ex) {
-			log.error("SoX could not create the open document file");
-		}
-		log.info("genericTransformAudioSrcToAudioDest end");
-		log.info(f.length());
-	
-		return ByteArrayHelper.read(f);
-	}
+    public DataHandler transformMp3ToFlacDH(DataHandler input) {
+        log.info("transformMp3ToFlacDH begin ");
+        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".flac",
+                null);
+    }
 
-	public DataHandler genericTransformAudioSrcToAudioDestDH(DataHandler input,
-			String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
-		log
-				.info("genericTransformAudioSrcToAudioDestDH begin : Converting from "
-						+ srcSuffix + " to " + destSuffix);
-		File f = FileUtils.tempFile("tempout.audio", destSuffix);
-		byte[] raw = null;
-		try {
-			FileOutputStream fos = new FileOutputStream(f);
-			log.info(input.getContentType());
-			log.info(raw);
-			log.info(input.getContent().toString());
-			log.info(raw);
-			fos.write(raw);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		File inputFile = FileUtils.tempFile(raw, "tempin.audio", srcSuffix);
-		if (!f.canRead() || !inputFile.exists() || !f.exists() || !f.canWrite()
-				|| !inputFile.canRead() || !inputFile.canWrite()) {
-			throw new IllegalStateException("Can't read from or write to: "
-					+ f.getAbsolutePath());
-		}
-		try {
-			List<String> commands = Arrays.asList(SOX, inputFile
-					.getAbsolutePath(), f.getAbsolutePath());
-			File home = new File(SOX_HOME);
-			commands.addAll(soxCliParams);
-			ProcessRunner pr = new ProcessRunner(commands);
-			pr.setStartingDir(home);
-			log.info("Executing: " + commands);
-			pr.run();
-			log.info("SOX call output: " + pr.getProcessOutputAsString());
-			log.error("SOX call error: " + pr.getProcessErrorAsString());
-			log.debug("Executing: " + commands + " finished.");
-		} catch (Exception ex) {
-			log.error("SoX could not create the open document file");
-		}
-		log.info("genericTransformAudioSrcToAudioDestDH end");
-		log.info(f.length());
-		return new DataHandler(new ByteArrayDataSource(ByteArrayHelper.read(f),
-				"application/octet-stream"));
-	}
+    public byte[] genericTransformAudioSrcToAudioDest(byte[] input,
+            String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
+
+        if (!srcSuffix.startsWith("."))
+            srcSuffix = "." + srcSuffix;
+        if (!destSuffix.startsWith("."))
+            destSuffix = "." + destSuffix;
+        log.info("genericTransformAudioSrcToAudioDest begin: Converting from "
+                + srcSuffix + " to " + destSuffix);
+        File f = FileUtils.tempFile("tempout", destSuffix);
+
+        File inputFile = FileUtils.tempFile(input, "tempin.audio", srcSuffix);
+        if (!f.canRead() || !inputFile.exists() || !f.exists() || !f.canWrite()
+                || !inputFile.canRead() || !inputFile.canWrite()) {
+            throw new IllegalStateException("Can't read from or write to: "
+                    + f.getAbsolutePath());
+        }
+
+        try {
+            List<String> commands = Arrays.asList(SOX, inputFile
+                    .getAbsolutePath(), f.getAbsolutePath());
+            commands.addAll(soxCliParams);
+            File home = new File(SOX_HOME);
+
+            ProcessRunner pr = new ProcessRunner(commands);
+            pr.setStartingDir(home);
+            log.info("Executing: " + commands);
+            pr.run();
+
+            log.info("SOX call output: " + pr.getProcessOutputAsString());
+            log.error("SOX call error: " + pr.getProcessErrorAsString());
+            log.debug("Executing: " + commands + " finished.");
+
+        } catch (Exception ex) {
+            log.error("SoX could not create the open document file");
+        }
+        log.info("genericTransformAudioSrcToAudioDest end");
+        log.info(f.length());
+
+        return ByteArrayHelper.read(f);
+    }
+
+    public DataHandler genericTransformAudioSrcToAudioDestDH(DataHandler input,
+            String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
+        log
+                .info("genericTransformAudioSrcToAudioDestDH begin : Converting from "
+                        + srcSuffix + " to " + destSuffix);
+        File f = FileUtils.tempFile("tempout.audio", destSuffix);
+        byte[] raw = null;
+        try {
+            FileOutputStream fos = new FileOutputStream(f);
+            log.info(input.getContentType());
+            log.info(raw);
+            log.info(input.getContent().toString());
+            log.info(raw);
+            fos.write(raw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File inputFile = FileUtils.tempFile(raw, "tempin.audio", srcSuffix);
+        if (!f.canRead() || !inputFile.exists() || !f.exists() || !f.canWrite()
+                || !inputFile.canRead() || !inputFile.canWrite()) {
+            throw new IllegalStateException("Can't read from or write to: "
+                    + f.getAbsolutePath());
+        }
+        try {
+            List<String> commands = Arrays.asList(SOX, inputFile
+                    .getAbsolutePath(), f.getAbsolutePath());
+            File home = new File(SOX_HOME);
+            if (soxCliParams != null) {
+                commands.addAll(soxCliParams);
+            }
+            ProcessRunner pr = new ProcessRunner(commands);
+            pr.setStartingDir(home);
+            log.info("Executing: " + commands);
+            pr.run();
+            log.info("SOX call output: " + pr.getProcessOutputAsString());
+            log.error("SOX call error: " + pr.getProcessErrorAsString());
+            log.debug("Executing: " + commands + " finished.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            log.error("SoX could not create the open document file");
+        }
+        log.info("genericTransformAudioSrcToAudioDestDH end");
+        log.info(f.length());
+        return new DataHandler(new ByteArrayDataSource(ByteArrayHelper.read(f),
+                "application/octet-stream"));
+    }
 
 }
--- trunk/PA/sox/test/java/eu/planets_project/services/migration/soxservices/BasicMigrateOneAudioBinaryTests.java
+++ trunk/PA/sox/test/java/eu/planets_project/services/migration/soxservices/BasicMigrateOneAudioBinaryTests.java
@@ -1,5 +1,7 @@
 package eu.planets_project.services.migration.soxservices;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
@@ -17,198 +19,195 @@ import javax.activation.FileDataSource;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
-import org.hibernate.validator.AssertFalse;
-import static org.junit.Assert.*;
 import org.junit.Test;
 
 import eu.planets_project.services.PlanetsException;
 import eu.planets_project.services.PlanetsServices;
 import eu.planets_project.services.migrate.BasicMigrateOneBinary;
-import eu.planets_project.services.migration.soxservices.MP3ToFlacSox;
-import eu.planets_project.services.migration.soxservices.MP3ToOggSox;
-import eu.planets_project.services.migration.soxservices.MP3ToWavSox;
-import eu.planets_project.services.migration.soxservices.WavToFlacSox;
-import eu.planets_project.services.migration.soxservices.WavToOggSox;
 
 public class BasicMigrateOneAudioBinaryTests {
 
-	public void test(BasicMigrateOneBinary converter, String srcFormat,
-			String destformat) {
-		try {
-			System.out.println("Sourceformat: " + srcFormat);
-			System.out.println("Targetformat: " + destformat);
-			String fileName = "/var/www/ab" + srcFormat;
-			DataSource ds = new FileDataSource(fileName);
-			DataHandler dataHandler = new DataHandler(ds);
+    public void test(BasicMigrateOneBinary converter, String srcFormat,
+            String destformat) {
+        try {
+            System.out.println("Sourceformat: " + srcFormat);
+            System.out.println("Targetformat: " + destformat);
+            String fileName = "PA/sox/test/resources/Arkansas" + srcFormat;
+            DataSource ds = new FileDataSource(fileName);
+            DataHandler dataHandler = new DataHandler(ds);
 
-			File srcFile = new File(fileName);
-			if (srcFile.exists() && srcFile.canRead()) {
-				System.out.println("OK.");
-				System.out.println(srcFile.getAbsolutePath());
-			}
-			System.out.println("creating Byte[]");
-			byte[] imageData = getByteArrayFromFile(srcFile);
-			System.out.println("Sending audio data...");
-			System.out.println(imageData.length + " Byte");
-			System.out.println(converter.QNAME + " Class: "
-					+ converter.getClass().getName());
-			byte[] resdh_orig = converter.basicMigrateOneBinary(imageData);
+            File srcFile = new File(fileName);
+            if (srcFile.exists() && srcFile.canRead()) {
+                System.out.println("OK.");
+                System.out.println(srcFile.getAbsolutePath());
+            }
+            System.out.println("creating Byte[]");
+            byte[] imageData = getByteArrayFromFile(srcFile);
+            System.out.println("data byta array has length: "
+                    + imageData.length);
+            System.out.println("Sending audio data...");
+            System.out.println(imageData.length + " Byte");
+            System.out.println(converter.QNAME + " Class: "
+                    + converter.getClass().getName());
+            byte[] resdh_orig = converter.basicMigrateOneBinary(imageData);
+            assertTrue("Sox returned null data;", resdh_orig != null);
+            System.out
+                    .println("Service executed, resulting byte array has length: "
+                            + resdh_orig.length);
+            System.out.println("Byte [] decoded...");
+            File testdir = new File(System.getProperty("user.home")
+                    + System.getProperty("file.separator") + "soundconversion");
+            if (!testdir.exists())
+                testdir.mkdir();
+            File resultFile = new File(testdir, "converted" + destformat);
+            if (!resultFile.exists()) {
+                resultFile.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(resultFile);
+            fos.write(resdh_orig);
+            fos.flush();
+            fos.close();
+            System.out.println(resultFile.getAbsolutePath() + " has "
+                    + resultFile.length() + "  bytes.");
+            assertFalse("Result file is empty", resultFile.length() == 0);
+        } catch (MalformedURLException e) {
+            fail(e.getMessage());
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
 
-			System.out.println("Service executed...");
-			System.out.println("Byte [] decoded...");
-			File testdir = new File(System.getProperty("user.home")
-					+ System.getProperty("file.separator") + "soundconversion");
-			if (!testdir.exists())
-				testdir.mkdir();
-			File resultFile = new File(testdir, "converted" + destformat);
-			if (!resultFile.exists()) {
-				resultFile.createNewFile();
-			}
-			FileOutputStream fos = new FileOutputStream(resultFile);
-			fos.write(resdh_orig);
-			fos.flush();
-			fos.close();
-			System.out.println(resultFile.getAbsolutePath() + " has "
-					+ resultFile.length() + "  bytes.");
-			assertFalse("Result file is empty",resultFile.length()==0);
-		} catch (MalformedURLException e) {
-			fail(e.getMessage());
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
+            fail(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
 
-			fail(e.getMessage());
-			e.printStackTrace();
-		} catch (IOException e) {
+            fail(e.getMessage());
+            e.printStackTrace();
+        } catch (PlanetsException e) {
 
-			fail(e.getMessage());
-			e.printStackTrace();
-		} catch (PlanetsException e) {
+            fail(e.getMessage());
+            e.printStackTrace();
+        }
 
-			fail(e.getMessage());
-			e.printStackTrace();
-		}
+    }
 
-	}
+    /**
+     * 
+     * 23.07.2008 13:31:10
+     */
+    @Test
+    public void localTests() {
+        System.out.println("**********************************************");
 
-	/**
-	 * 
-	 * 23.07.2008 13:31:10
-	 */
-	@Test
-	public void localTests() {
-		System.out.println("**********************************************");
+        test(new MP3ToWavSox(), ".mp3", "local.wav");
+        System.out.println("**********************************************");
 
-		test(new MP3ToWavSox(), ".mp3", "local.wav");
-		System.out.println("**********************************************");
+        test(new MP3ToOggSox(), ".mp3", "local.ogg");
+        System.out.println("**********************************************");
 
-		test(new MP3ToOggSox(), ".mp3", "local.ogg");
-		System.out.println("**********************************************");
+        test(new MP3ToFlacSox(), ".mp3", "local.flac");
+        System.out.println("**********************************************");
 
-		test(new MP3ToFlacSox(), ".mp3", "local.flac");
-		System.out.println("**********************************************");
+        test(new WavToFlacSox(), ".wav", "local2.flac");
+        System.out.println("**********************************************");
 
-		test(new WavToFlacSox(), ".wav", "local2.flac");
-		System.out.println("**********************************************");
+        test(new WavToOggSox(), ".wav", "local2.ogg");
+        System.out.println("**********************************************");
 
-		test(new WavToOggSox(), ".wav", "local2.ogg");
-		System.out.println("**********************************************");
+    }
 
-	}
+    @Test
+    public void clientTest() {
+        try {
+            Service service = null;
+            BasicMigrateOneBinary servicePort = null;
 
-	@Test
-	public void clientTest() {
-		try {
-			Service service = null;
-			BasicMigrateOneBinary servicePort = null;
+            System.out
+                    .println("**********************************************");
 
-			System.out
-					.println("**********************************************");
+            service = Service.create(new URL(
+                    "http://localhost:8080/pserv-pa-sox/MP3ToOggSox?wsdl"),
+                    new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
+            servicePort = service.getPort(BasicMigrateOneBinary.class);
+            test(servicePort, ".mp3", ".ogg");
 
-			service = Service.create(new URL(
-					"http://localhost:8080/pserv-pa-sox/MP3ToOggSox?wsdl"),
-					new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
-			servicePort = service.getPort(BasicMigrateOneBinary.class);
-			test(servicePort, ".mp3", ".ogg");
+            System.out
+                    .println("**********************************************");
 
-			System.out
-					.println("**********************************************");
+            service = Service.create(new URL(
+                    "http://localhost:8080/pserv-pa-sox/MP3ToWavSox?wsdl"),
+                    new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
+            servicePort = service.getPort(BasicMigrateOneBinary.class);
+            test(servicePort, ".mp3", ".wav");
 
-			service = Service.create(new URL(
-					"http://localhost:8080/pserv-pa-sox/MP3ToWavSox?wsdl"),
-					new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
-			servicePort = service.getPort(BasicMigrateOneBinary.class);
-			test(servicePort, ".mp3", ".wav");
+            System.out
+                    .println("**********************************************");
 
-			System.out
-					.println("**********************************************");
+            service = Service.create(new URL(
+                    "http://localhost:8080/pserv-pa-sox/MP3ToFlacSox?wsdl"),
+                    new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
+            servicePort = service.getPort(BasicMigrateOneBinary.class);
+            test(servicePort, ".mp3", ".flac");
 
-			service = Service.create(new URL(
-					"http://localhost:8080/pserv-pa-sox/MP3ToFlacSox?wsdl"),
-					new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
-			servicePort = service.getPort(BasicMigrateOneBinary.class);
-			test(servicePort, ".mp3", ".flac");
+            System.out
+                    .println("**********************************************");
 
-			System.out
-					.println("**********************************************");
+            service = Service.create(new URL(
+                    "http://localhost:8080/pserv-pa-sox/WavToOggSox?wsdl"),
+                    new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
+            servicePort = service.getPort(BasicMigrateOneBinary.class);
+            test(servicePort, ".wav", "2.ogg");
 
-			service = Service.create(new URL(
-					"http://localhost:8080/pserv-pa-sox/WavToOggSox?wsdl"),
-					new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
-			servicePort = service.getPort(BasicMigrateOneBinary.class);
-			test(servicePort, ".wav", "2.ogg");
+            System.out
+                    .println("**********************************************");
 
-			System.out
-					.println("**********************************************");
+            service = Service.create(new URL(
+                    "http://localhost:8080/pserv-pa-sox/WavToFlacSox?wsdl"),
+                    new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
+            servicePort = service.getPort(BasicMigrateOneBinary.class);
+            test(servicePort, ".wav", "2.flac");
 
-			service = Service.create(new URL(
-					"http://localhost:8080/pserv-pa-sox/WavToFlacSox?wsdl"),
-					new QName(PlanetsServices.NS, BasicMigrateOneBinary.NAME));
-			servicePort = service.getPort(BasicMigrateOneBinary.class);
-			test(servicePort, ".wav", "2.flac");
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
 
-		} catch (IOException e) {
-			fail(e.getMessage());
-		}
+    }
 
-	}
+    private static byte[] getByteArrayFromFile(File file) throws IOException {
+        InputStream is = new FileInputStream(file);
 
-	private static byte[] getByteArrayFromFile(File file) throws IOException {
-		InputStream is = new FileInputStream(file);
+        // Get the size of the file
+        long length = file.length();
 
-		// Get the size of the file
-		long length = file.length();
+        // You cannot create an array using a long type.
+        // It needs to be an int type.
+        // Before converting to an int type, check
+        // to ensure that file is not larger than Integer.MAX_VALUE.
+        if (length > Integer.MAX_VALUE) {
+            // throw new
+            // IllegalArgumentException("getBytesFromFile@JpgToTiffConverter::
+            // The file is too large (i.e. larger than 2 GB!");
+            System.out.println("Datei ist zu gross (e.g. groesser als 2GB)!");
+        }
 
-		// You cannot create an array using a long type.
-		// It needs to be an int type.
-		// Before converting to an int type, check
-		// to ensure that file is not larger than Integer.MAX_VALUE.
-		if (length > Integer.MAX_VALUE) {
-			// throw new
-			// IllegalArgumentException("getBytesFromFile@JpgToTiffConverter::
-			// The file is too large (i.e. larger than 2 GB!");
-			System.out.println("Datei ist zu gross (e.g. groesser als 2GB)!");
-		}
+        // Create the byte array to hold the data
+        byte[] bytes = new byte[(int) length];
 
-		// Create the byte array to hold the data
-		byte[] bytes = new byte[(int) length];
+        // Read in the bytes
+        int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length
+                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+            offset += numRead;
+        }
 
-		// Read in the bytes
-		int offset = 0;
-		int numRead = 0;
-		while (offset < bytes.length
-				&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-			offset += numRead;
-		}
+        // Ensure all the bytes have been read in
+        if (offset < bytes.length) {
+            throw new IOException("Could not completely read file "
+                    + file.getName());
+        }
 
-		// Ensure all the bytes have been read in
-		if (offset < bytes.length) {
-			throw new IOException("Could not completely read file "
-					+ file.getName());
-		}
-
-		// Close the input stream and return bytes
-		is.close();
-		return bytes;
-	}
+        // Close the input stream and return bytes
+        is.close();
+        return bytes;
+    }
 
 }
