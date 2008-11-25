6db8b77117c10768a37eeea15564d83c098de7d4
Georg Petz <georg.petz@onb.ac.at>
2008-11-25 11:26:25 +0000

    gimp: jpg -> png

--- trunk/PA/gimp/src/java/eu/planets_project/services/migration/gimp/Gimp26Migration.java
+++ trunk/PA/gimp/src/java/eu/planets_project/services/migration/gimp/Gimp26Migration.java
@@ -35,8 +35,6 @@ import eu.planets_project.services.utils.ByteArrayHelper;
 import eu.planets_project.services.utils.PlanetsLogger;
 import eu.planets_project.services.utils.ProcessRunner;
 
-
-
 /**
  * The Gimp26Migration class migrates various image file formats. 
  * This class is a wrapper for The GIMP version 2.6.
@@ -46,26 +44,20 @@ import eu.planets_project.services.utils.ProcessRunner;
 @Local(Migrate.class)
 @Remote(Migrate.class)
 @Stateless
-@WebService(/* name = Gimp26Migration.NAME ,*/ 
-        serviceName = Migrate.NAME, 
-        targetNamespace = PlanetsServices.NS,
-        endpointInterface = "eu.planets_project.services.migrate.Migrate" )
+@WebService(/* name = Gimp26Migration.NAME ,*/serviceName = Migrate.NAME,
+targetNamespace = PlanetsServices.NS,
+endpointInterface = "eu.planets_project.services.migrate.Migrate")
 public final class Gimp26Migration implements Migrate, Serializable {
-    
+
     PlanetsLogger log = PlanetsLogger.getLogger(Gimp26Migration.class);
-    
     private static Logger logger = Logger.getLogger(Gimp26Migration.class.getName());
-    
     public String gimp_install_dir;
     public String gimp_app_name;
     public String gimp_outfile_ext;
-    
     private File tmpInFile;
     private File tmpOutFile;
-    
     /***/
     static final String NAME = "Gimp26Migration";
-    
     /***/
     private static final long serialVersionUID = 2127494848765937613L;
 
@@ -74,120 +66,113 @@ public final class Gimp26Migration implements Migrate, Serializable {
      * 
      * @see eu.planets_project.ifr.core.common.services.migrate.MigrateOneDigitalObject#migrate(eu.planets_project.ifr.core.common.services.datatypes.DigitalObject)
      */
-    public MigrateResult migrate( final DigitalObject digitalObject, URI inputFormat,
+    public MigrateResult migrate(final DigitalObject digitalObject, URI inputFormat,
             URI outputFormat, Parameters parameters) {
-        
+
+        System.out.println(inputFormat.toString());
+        System.out.println(outputFormat.toString());
+
         Properties props = new Properties();
         try {
             String strRsc = "/eu/planets_project/services/migration/gimp/gimp26.properties";
-            props.load( this.getClass().getResourceAsStream(strRsc));
+            props.load(this.getClass().getResourceAsStream(strRsc));
             // config vars
             this.gimp_install_dir = props.getProperty("gimp.install.dir");
             this.gimp_app_name = props.getProperty("gimp.app.name");
             this.gimp_outfile_ext = props.getProperty("gimp.outfile.ext");
-             
-        } catch( Exception e ) {
+
+        } catch (Exception e) {
             // // config vars
-            this.gimp_install_dir  = "/home/georg/Projects/PLANETS/";
+            this.gimp_install_dir = "/home/georg/Projects/PLANETS/";
             this.gimp_app_name = "dmmConvertPNGtoJPG.sh";
             this.gimp_outfile_ext = "tmp";
         }
-        log.info("Using gimp install directory: "+this.gimp_install_dir);
-        log.info("Using gimp application name: "+this.gimp_app_name);
-        log.info("Using gimp application name: "+this.gimp_app_name);
-        
+        log.info("Using gimp install directory: " + this.gimp_install_dir);
+        log.info("Using gimp application name: " + this.gimp_app_name);
+        log.info("Using gimp application name: " + this.gimp_app_name);
+
         /*
          * We just return a new digital object with the same required arguments
          * as the given:
          */
         byte[] binary = digitalObject.getContent().getValue();
-        
+
         try {
-            
+
             // write binary array to temporary file
-            writeByteArrayToTmpFile( binary );
-            
+            writeByteArrayToTmpFile(binary);
+
             // outfile name = infilename + ".ps" extension
-            String outFileStr = tmpInFile.getAbsolutePath()+"."+gimp_outfile_ext;
-            
+            String outFileStr = tmpInFile.getAbsolutePath() + "." + gimp_outfile_ext;
+
             // temporary outfile
             tmpOutFile = new File(outFileStr);
-            
+
             InputStream inputStream = new ByteArrayInputStream(binary);
-            
+
             ProcessRunner runner = new ProcessRunner();
             List<String> command = new ArrayList<String>();
-            
-            // setting up command
-            command.add(gimp_install_dir+gimp_app_name);
-            command.add(tmpInFile.getAbsolutePath());
-            command.add(tmpInFile.getAbsolutePath()+".jpg");
-            
-            /*command.add("-c");
-            command.add("-i");
-            command.add("-d");
-            
-            command.add("-b");
-            command.add("'(dmmConvertPNGtoJPG");
-            command.add("\""+tmpInFile.getAbsolutePath()+"\"");
-            command.add("\""+tmpInFile.getAbsolutePath()+".jpg\")'");
-            
-            command.add("-b");
-            command.add("'(gimp-quit 0)'");*/
-            
+
+            if (inputFormat.equals(Format.extensionToURI("PNG")) && outputFormat.equals(Format.extensionToURI("JPG"))) {
+                command.add(gimp_install_dir + gimp_app_name);
+                command.add(tmpInFile.getAbsolutePath());
+                command.add(tmpInFile.getAbsolutePath() + ".jpg");
+            } else if (inputFormat.equals(Format.extensionToURI("JPG")) && outputFormat.equals(Format.extensionToURI("PNG"))) {
+                command.add(gimp_install_dir + "dmmConvertJPGtoPNG.sh");
+                command.add(tmpInFile.getAbsolutePath());
+                command.add(tmpInFile.getAbsolutePath() + ".png");
+            }
+
             runner.setCommand(command);
             runner.setInputStream(inputStream);
-            
+
             runner.run();
-            
+
             int return_code = runner.getReturnCode();
 
-            if (return_code != 0){
+            if (return_code != 0) {
                 log.error("Gimp conversion error code: " + Integer.toString(return_code));
             }
-            
+
             // read byte array from temporary file
-            if( tmpInFile.isFile() && tmpInFile.canRead() )
-                binary = readByteArrayFromTmpFile(); 
-            else
-                log.error( "Error: Unable to read temporary file "
-                        +tmpInFile.getPath()+tmpInFile.getName() );
-            
-        } catch(IOException e) {
-            log.error( "IO Error:" + e.toString() );
+            if (tmpInFile.isFile() && tmpInFile.canRead()) {
+                binary = readByteArrayFromTmpFile();
+            } else {
+                log.error("Error: Unable to read temporary file " + tmpInFile.getPath() + tmpInFile.getName());
+            }
+        } catch (IOException e) {
+            log.error("IO Error:" + e.toString());
         } finally {
-            
         }
         DigitalObject newDO = null;
-        
+
         ServiceReport report = new ServiceReport();
-        
+
         newDO = new DigitalObject.Builder(Content.byValue(binary)).build();
-        
+
         return new MigrateResult(newDO, report);
     }
 
-    
     /* (non-Javadoc)
      * @see eu.planets_project.ifr.core.common.services.migrate.MigrateOneDigitalObject#describe()
      */
     public ServiceDescription describe() {
-        
+
         // parameters
         List<Parameter> parameterList = new ArrayList<Parameter>();
         Parameter compressionTypeParam = new Parameter("compressionType", "0-10");
         compressionTypeParam.setDescription("Allowed int values: 0 - 10");
         parameterList.add(compressionTypeParam);
-        
+
         Parameter compressionLevelParam = new Parameter("compressionQuality", "0-100");
         compressionLevelParam.setDescription("This should be an int value between: 0 - 100, representing the compression quality in percent.");
         parameterList.add(compressionLevelParam);
-        
+
         Parameters parameters = new Parameters();
         parameters.setParameters(parameterList);
-        
+
         // input formats
-        List<String> inputFormats = new ArrayList<String> ();
+        List<String> inputFormats = new ArrayList<String>();
         inputFormats.add("JPEG");
         inputFormats.add("TIFF");
         inputFormats.add("GIF");
@@ -198,60 +183,53 @@ public final class Gimp26Migration implements Migrate, Serializable {
         inputFormats.add("TGA");
         inputFormats.add("PCD");
         inputFormats.add("PDF");
-        
+
         // output formats
-        List<String> outputFormats = new ArrayList<String> ();
+        List<String> outputFormats = new ArrayList<String>();
         outputFormats.add("TIFF");
         outputFormats.add("PNG");
         outputFormats.add("JPEG");
         outputFormats.add("GIF");
         outputFormats.add("PDF");
-        
-        ServiceDescription mds = new ServiceDescription.Builder(NAME, Migrate.class.getName())
-                .author("Sven Schlarb <shsschlarb-planets@yahoo.de>, Georg Petz <georg.petz@onb.ac.at>")
-                .classname(this.getClass().getCanonicalName())
-                .description("Wrapper for GIMP version 2.6")
-                .version("0.1")
-                .parameters(parameters)
-                .paths(createMigrationPathwayMatrix(inputFormats, outputFormats))
-                .build();
+
+        ServiceDescription mds = new ServiceDescription.Builder(NAME, Migrate.class.getName()).author("Sven Schlarb <shsschlarb-planets@yahoo.de>, Georg Petz <georg.petz@onb.ac.at>").classname(this.getClass().getCanonicalName()).description("Wrapper for GIMP version 2.6").version("0.1").parameters(parameters).paths(createMigrationPathwayMatrix(inputFormats, outputFormats)).build();
         return mds;
     }
-    
+
     /* (non-Javadoc)
      */
-    synchronized void writeByteArrayToTmpFile( byte[] binary ) throws IOException {
-            tmpInFile = ByteArrayHelper.write(binary);
-            if( tmpInFile.exists() )
-                log.info("Temporary input file created: " + tmpInFile.getAbsolutePath());
-            else
-                log.error("Unable to create temp file");
+    synchronized void writeByteArrayToTmpFile(byte[] binary) throws IOException {
+        tmpInFile = ByteArrayHelper.write(binary);
+        if (tmpInFile.exists()) {
+            log.info("Temporary input file created: " + tmpInFile.getAbsolutePath());
+        } else {
+            log.error("Unable to create temp file");
+        }
     }
-    
+
     /* (non-Javadoc)
      */
     synchronized byte[] readByteArrayFromTmpFile() throws IOException {
-        String strOutFile = tmpInFile.getAbsolutePath()+"."+gimp_outfile_ext;
+        String strOutFile = tmpInFile.getAbsolutePath() + "." + gimp_outfile_ext;
         tmpOutFile = new File(strOutFile);
         byte[] binary = ByteArrayHelper.read(tmpInFile);
         return binary;
     }
-    
-    private MigrationPath[] createMigrationPathwayMatrix (List<String> inputFormats, List<String> outputFormats) {
+
+    private MigrationPath[] createMigrationPathwayMatrix(List<String> inputFormats, List<String> outputFormats) {
         List<MigrationPath> paths = new ArrayList<MigrationPath>();
 
         for (Iterator iterator = inputFormats.iterator(); iterator.hasNext();) {
             String input = (String) iterator.next();
 
             for (Iterator iterator2 = outputFormats.iterator(); iterator2.hasNext();) {
-                    String output = (String) iterator2.next();
-                    MigrationPath path = new MigrationPath(Format.extensionToURI(input), 
-                            Format.extensionToURI(output), null);
-                    
-                    paths.add(path);
+                String output = (String) iterator2.next();
+                MigrationPath path = new MigrationPath(Format.extensionToURI(input),
+                        Format.extensionToURI(output), null);
+
+                paths.add(path);
             }
         }
         return paths.toArray(new MigrationPath[]{});
     }
-
 }
--- trunk/PA/gimp/test/eu/planets_project/services/migration/gimp/Gimp26MigrationTest.java
+++ trunk/PA/gimp/test/eu/planets_project/services/migration/gimp/Gimp26MigrationTest.java
@@ -1,5 +1,6 @@
 package eu.planets_project.services.migration.gimp;
 
+import eu.planets_project.ifr.core.techreg.api.formats.Format;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -27,7 +28,7 @@ import eu.planets_project.services.utils.test.ServiceCreator;
  * @author Sven Schlarb <shsschlarb-planets@yahoo.de>, Georg Petz <georg.petz@onb.ac.at>
  */
 public final class Gimp26MigrationTest {
-    
+
     /* The location of this service when deployed. */
     String wsdlLoc = "/pserv-pa-gimp/Gimp26MigrationNew?wsdl";
 
@@ -40,9 +41,10 @@ public final class Gimp26MigrationTest {
      * 
      * @see junit.framework.TestCase#setUp()
      */
+
     @Before
     public void setUp() throws Exception {
-        dom = ServiceCreator.createTestService(Migrate.QNAME, Gimp26Migration.class, wsdlLoc );
+        dom = ServiceCreator.createTestService(Migrate.QNAME, Gimp26Migration.class, wsdlLoc);
     }
 
     /*
@@ -62,81 +64,98 @@ public final class Gimp26MigrationTest {
         ServiceDescription desc = dom.describe();
         System.out.println("Received service description: ");
         System.out.println(desc.toXml());
-        assertTrue("The ServiceDescription should not be NULL.", desc != null );
+        assertTrue("The ServiceDescription should not be NULL.", desc != null);
     }
 
-    /**
-     * Test the pass-thru migration.
-     */
     @Test
-    public void testMigrate() throws IOException {
+    public void testMigratePNGtoJPG() throws IOException {
         try {
             /*
              * To test usability of the digital object instance in web services,
              * we simply pass one into the service and expect one back:
              */
-            byte[] binary = this.readByteArrayFromFile();
-            DigitalObject input = new DigitalObject.Builder( Content.byValue(binary))
-                    .build();
+            byte[] binary = this.readByteArrayFromFile("PA/gimp/test/testfiles/demonstration.png");
+            DigitalObject input = new DigitalObject.Builder(Content.byValue(binary)).build();
 
-            MigrateResult mr = dom.migrate(input, null, null, null);
+            MigrateResult mr = dom.migrate(input, Format.extensionToURI("PNG"), Format.extensionToURI("JPG"), null);
             DigitalObject doOut = mr.getDigitalObject();
 
             assertTrue("Resulting digital object is null.", doOut != null);
 
-            
-            writeByteArrayToFile(doOut.getContent().getValue());
-            
+
+            writeByteArrayToFile(doOut.getContent().getValue(), "PA/gimp/test/testfiles/demonstrationFromPNG.jpg");
+
 
         } catch (MalformedURLException e) {
-            fail("Malformed URL exception: "+e.toString());
+            fail("Malformed URL exception: " + e.toString());
         }
 
     }
-    
-    synchronized byte[] readByteArrayFromFile() 
+
+    @Test
+    public void testMigrateJPGtoPNG() throws IOException {
+        try {
+            /*
+             * To test usability of the digital object instance in web services,
+             * we simply pass one into the service and expect one back:
+             */
+            byte[] binary = this.readByteArrayFromFile("PA/gimp/test/testfiles/demonstration.jpg");
+            DigitalObject input = new DigitalObject.Builder(Content.byValue(binary)).build();
+
+            MigrateResult mr = dom.migrate(input, Format.extensionToURI("PNG"), Format.extensionToURI("JPG"), null);
+            DigitalObject doOut = mr.getDigitalObject();
+
+            assertTrue("Resulting digital object is null.", doOut != null);
+
+
+            writeByteArrayToFile(doOut.getContent().getValue(), "PA/gimp/test/testfiles/demonstrationFromJPG.png");
+
+
+        } catch (MalformedURLException e) {
+            fail("Malformed URL exception: " + e.toString());
+        }
+
+    }
+
+    synchronized byte[] readByteArrayFromFile(String strInFile)
             throws IOException {
         byte[] binary = new byte[0];
-        
-        String strOutFile = "PA/gimp/test/testfiles/demonstration.png";
+
+        //String strOutFile = "PA/gimp/test/testfiles/demonstration.png";
         //String strOutFile = "test/testfiles/testin.dvi";
-        fTmpOutFile = new File(strOutFile);
-        assertTrue("DVI input file "+fTmpOutFile.getAbsolutePath()+" does not exist.", fTmpOutFile.exists());
+        fTmpOutFile = new File(strInFile);
+        assertTrue("input file " + fTmpOutFile.getAbsolutePath() + " does not exist.", fTmpOutFile.exists());
         try {
-            if( fTmpOutFile.isFile() && fTmpOutFile.canRead())
-            {
-                binary = new byte[(int)fTmpOutFile.length()];
+            if (fTmpOutFile.isFile() && fTmpOutFile.canRead()) {
+                binary = new byte[(int) fTmpOutFile.length()];
                 FileInputStream fis = new FileInputStream(fTmpOutFile);
                 fis.read(binary);
                 System.out.println("Read file: " + fTmpOutFile.getAbsolutePath());
                 fis.close();
+            } else {
+                fail("Unable to read file: " + fTmpOutFile.getAbsolutePath());
             }
-            else
-            {
-                fail("Unable to read file: "+fTmpOutFile.getAbsolutePath());
-            }
-        } catch(IOException ex) {
-            fail("IO Error: "+ex.toString());
+        } catch (IOException ex) {
+            fail("IO Error: " + ex.toString());
         }
         return binary;
     }
-    
-    synchronized void writeByteArrayToFile( byte[] binary )
+
+    synchronized void writeByteArrayToFile(byte[] binary, String strOutFile)
             throws IOException {
         try {
-            String strOutFile = "PA/gimp/test/testfiles/demonstration.jpg";
+            //String strOutFile = "PA/gimp/test/testfiles/demonstration.jpg";
             //String strOutFile = "test/testfiles/testout.ps";
             this.fTmpInFile = new File(strOutFile);
             System.out.println();
-            BufferedOutputStream fos = 
-                            new BufferedOutputStream(
-                            new FileOutputStream(fTmpInFile));
+            BufferedOutputStream fos =
+                    new BufferedOutputStream(
+                    new FileOutputStream(fTmpInFile));
             fos.write(binary);
             fos.close();
             assertTrue("Output file has not been created correctly. ", fTmpInFile.exists() && fTmpInFile.length() > 0);
-        } catch(IOException ex) {
-            fail("IO Error: "+ex.toString());
+        } catch (IOException ex) {
+            fail("IO Error: " + ex.toString());
         }
     }
-
 }
