35c8615edcf14bb565617924e75c594998a9c644
shsdev <shsdev@unknown.com>
2008-12-09 16:22:54 +0000

    Update of GIMP Service Wrapper Implementation

--- trunk/PA/gimp/src/java/eu/planets_project/services/migration/gimp/Gimp26Migration.java
+++ trunk/PA/gimp/src/java/eu/planets_project/services/migration/gimp/Gimp26Migration.java
@@ -32,6 +32,7 @@ import eu.planets_project.services.utils.ByteArrayHelper;
 import eu.planets_project.services.utils.PlanetsLogger;
 import eu.planets_project.services.utils.ProcessRunner;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -65,6 +66,10 @@ public final class Gimp26Migration implements Migrate, Serializable {
     List<String> inputFormats = null;
     HashMap  outputFormats = null;
     HashMap  formatMapping = null;
+    HashMap defaultParameters = null;
+    
+    String inputFmtExt = null;
+    String outputFmtExt = null;
     
     private void init()
     {
@@ -79,34 +84,157 @@ public final class Gimp26Migration implements Migrate, Serializable {
         inputFormats.add("PS");
         inputFormats.add("TIFF");
         inputFormats.add("BMP");
-
-        // output formats and associated output parameters
-        outputFormats = new HashMap();
-        // options: interlace dither palette num-colors alpha-dither remove-unused
-        outputFormats.put("GIF"," 1 0 256 0 0"); 
-        // options:  width height x-offset y-offset unit keep-ratio rotation 
-        // preview level
-        outputFormats.put("EPS"," 0 0 0 0 0 1 0 0 2"); 
-        // options:  quality smoothing optimize progressive
-        outputFormats.put("JPEG"," 0.1 0 1 1"); 
-        // options: interlace compression
-        outputFormats.put("PNG"," 1 1"); 
-        // options:  width height x-offset y-offset unit keep-ratio rotation 
-        // preview level
-        outputFormats.put("PS"," 0 0 0 0 0 1 0 0 2"); 
-        // options: compression Compression type: {None (0), LZW (1), PACKBITS(2), 
-        // DEFLATE (3), JPEG (4), CCITT G3 Fax (5), CCITT G4 Fax (6)}
-        outputFormats.put("TIFF"," 1");
-        // options: none
-        outputFormats.put("BMP",""); 
         
+        // Disambiguation of extensions, e.g. {"JPG","JPEG"} to {"JPEG"}
         // FIXIT This should be supported by the FormatRegistryImpl class, but
         // it does not provide the complete set at the moment.
         formatMapping = new HashMap();
         formatMapping.put("JPG","JPEG");
         formatMapping.put("TIF","TIFF");
     }
-
+    
+    private void getExtensions(URI inputFormat, URI outputFormat)
+    {
+        inputFmtExt = getFormatExt( inputFormat, false );
+        outputFmtExt = getFormatExt( outputFormat, true );
+    }
+    
+    /**
+     * Create a white space separated parameter string which can be passed
+     * over to the gimp script (e.g.  1 0 256 0 0 for migration to GIF).
+     * 
+     * @param paramNameValuePair The data structure containing the parameter
+     * value pairs
+     * @return
+     */
+    private String getParameterString(HashMap paramNameValuePair)
+    {
+        StringBuffer paramStrBuff = new StringBuffer();
+        Collection values = paramNameValuePair.values();
+        Iterator itr = values.iterator();
+        while(itr.hasNext()) {
+            paramStrBuff.append(" ");
+            paramStrBuff.append((String) itr.next());
+        }
+        return paramStrBuff.toString();
+    }
+    
+    private void setParameters(Parameters parameters)
+    {
+        // output formats and associated output parameters
+        outputFormats = new HashMap();
+        
+        // main parameters hashmap
+        // e.g. 
+        // { "GIF" -> { "gif-interlace", "gif-dither", ... } }
+        // { "JPEG" -> { "jpeg-quality", "jpeg-smoothing", ... } }
+        // ...
+        defaultParameters = new HashMap();
+        /*
+        // Define parameters and default values
+        // GIF
+        List<Parameter> gifParameterList = new ArrayList<Parameter>();
+        Parameter gifInterlaceParam = new new Parameter("gif-interlace", "1", "xsd:integer { minLength=\"0\" maxLength=\"1\" }");
+        gifParameterList.add(gifInterlaceParam);
+        gifParameterList.add(new Parameter("gif-dither", "1", "xsd:integer { minLength=\"0\" maxLength=\"1\" }"));
+        gifParameterList.add(new Parameter("gif-numcolors", "256", "xsd:integer { minLength=\"0\" maxLength=\"256\" }"));
+        gifParameterList.add(new Parameter("gif-dither", "1", "xsd:integer { minLength=\"0\" maxLength=\"1\" }"));
+        gifParameterList.add(new Parameter("gif-alphadither", "1", "xsd:integer { minLength=\"0\" maxLength=\"1\" }"));
+        gifParameterList.add(new Parameter("gif-removeunused", "1", "xsd:integer { minLength=\"0\" maxLength=\"1\" }"));
+        defaultParameters.put("GIF", gifParameterList);
+        */
+        // EPS
+        HashMap epsNameValuePair = new HashMap();
+        epsNameValuePair.put("eps-width", "0");
+        epsNameValuePair.put("eps-height", "0");
+        epsNameValuePair.put("eps-xoffset", "0");
+        epsNameValuePair.put("eps-yoffset", "0");
+        epsNameValuePair.put("eps-unit", "0");
+        epsNameValuePair.put("eps-keepratio", "1");
+        epsNameValuePair.put("eps-rotation", "0");
+        epsNameValuePair.put("eps-preview", "0");
+        epsNameValuePair.put("eps-level", "2");
+        defaultParameters.put("EPS", epsNameValuePair);
+        
+        // JPEG
+        HashMap jpegNameValuePair = new HashMap();
+        jpegNameValuePair.put("jpeg-quality", "0.1");
+        jpegNameValuePair.put("jpeg-smoothing", "0");
+        jpegNameValuePair.put("jpeg-optimize", "1");
+        jpegNameValuePair.put("jpeg-progressive", "1");
+        defaultParameters.put("JPEG", jpegNameValuePair);
+        
+        // PNG
+        HashMap pngNameValuePair = new HashMap();
+        pngNameValuePair.put("png-interlace", "1");
+        pngNameValuePair.put("png-compression", "1");
+        defaultParameters.put("PNG", pngNameValuePair);
+        
+        // PS
+        HashMap psNameValuePair = new HashMap();
+        psNameValuePair.put("ps-width", "0");
+        psNameValuePair.put("ps-height", "0");
+        psNameValuePair.put("ps-xoffset", "0");
+        psNameValuePair.put("ps-yoffset", "0");
+        psNameValuePair.put("ps-unit", "0");
+        psNameValuePair.put("ps-keepratio", "1");
+        psNameValuePair.put("ps-rotation", "0");
+        psNameValuePair.put("ps-preview", "0");
+        psNameValuePair.put("ps-level", "2");
+        defaultParameters.put("PS", psNameValuePair);
+        
+        // TIFF
+        HashMap tiffNameValuePair = new HashMap();
+        tiffNameValuePair.put("tiff-compressiontype", "1");
+        defaultParameters.put("TIFF", tiffNameValuePair);
+        
+        // BMP
+        HashMap bmpNameValuePair = new HashMap();
+        bmpNameValuePair.put("bmp-dummy", "");
+        defaultParameters.put("BMP", bmpNameValuePair);
+        
+        // Change default parameters according to the parameters defined by the
+        // user
+        List<Parameter> paramList = parameters.getParameters(); // User parameters
+        Iterator itr = paramList.iterator(); 
+        while(itr.hasNext()) {
+            Parameter param = (Parameter) itr.next();
+            String paramName = param.name;
+            String paramValue = param.value;
+            System.out.println("Set parameter: " + paramName + " with value: " + paramValue);
+            HashMap hm = (HashMap)defaultParameters.get(outputFmtExt);
+            // is the parameter permitted?
+            if( hm.containsKey(paramName) )
+            {
+                hm.put(paramName, paramValue); // override default parameter
+            }
+        }
+        
+        // options: interlace dither palette num-colors alpha-dither remove-unused
+        // defaults: 1 0 256 0 0
+        //outputFormats.put("GIF",getParameterString(gifNameValuePair)); 
+        // options:  width height x-offset y-offset unit keep-ratio rotation 
+        // preview level
+        // defaults: 0 0 0 0 0 1 0 0 2
+        outputFormats.put("EPS",getParameterString(epsNameValuePair)); 
+        // options:  quality smoothing optimize progressive
+        // defaults: 0.1 0 1 1
+        outputFormats.put("JPEG",getParameterString(jpegNameValuePair)); 
+        // options: interlace compression
+        // defaults:  1 1
+        outputFormats.put("PNG",getParameterString(pngNameValuePair)); 
+        // options:  width height x-offset y-offset unit keep-ratio rotation 
+        // preview level
+        // default: 0 0 0 0 0 1 0 0 2
+        outputFormats.put("PS",getParameterString(psNameValuePair)); 
+        // options: compression Compression type: {None (0), LZW (1), PACKBITS(2), 
+        // DEFLATE (3), JPEG (4), CCITT G3 Fax (5), CCITT G4 Fax (6)}
+        // defaults:  1
+        outputFormats.put("TIFF",getParameterString(tiffNameValuePair));
+        // options: none
+        // defaults: 
+        outputFormats.put("BMP",getParameterString(bmpNameValuePair)); 
+    }
     /**
      * {@inheritDoc}
      * 
@@ -115,8 +243,20 @@ public final class Gimp26Migration implements Migrate, Serializable {
     public MigrateResult migrate(final DigitalObject digitalObject, URI inputFormat,
             URI outputFormat, Parameters parameters) {
         
-        // initialise variables
+        // initialise input formats (GIF, JPEG, EPS, ...) and output formats list 
+        // and (GIF, JPEG, EPS, ...) and apply disambiguation ("JPG" -> "JPEG"}
         init();
+        
+         // set input and output extensions based upon input and output format
+        // e.g. 
+        // inputFormat="planets:fmt/ext/tif" -> inputFmtExt="TIFF"
+        // outputFormat="planets:fmt/ext/jpg" -> outputFmtExt="JPEG"
+        getExtensions(inputFormat,outputFormat);
+        
+        // Set the parameterString depending on the desired output format
+        // which is first initialised by default values which are then
+        // overridden by user parameters of the service call.
+        setParameters(parameters);
         
         // configuration parameters
         Properties props = new Properties();
@@ -146,7 +286,7 @@ public final class Gimp26Migration implements Migrate, Serializable {
         InputStream inputStream = new ByteArrayInputStream(binary);
 
         // set gimp fu-script command
-        gimpFuScriptCmdStr = getFuScriptMigrationStr(inputFormat,outputFormat);
+        gimpFuScriptCmdStr = getFuScriptMigrationStr();
         System.out.println("GIMP Fu-Script command "+gimpFuScriptCmdStr);
         if( gimpFuScriptCmdStr != null )
         {
@@ -200,10 +340,23 @@ public final class Gimp26Migration implements Migrate, Serializable {
 
         // parameters
         // gif
+        // options: interlace dither palette num-colors alpha-dither remove-unused
         List<Parameter> parameterList = new ArrayList<Parameter>();
-        Parameter gifInterlaceParam = new Parameter("gifInterlace", "true/false");
+        Parameter gifInterlaceParam = new Parameter("gif-interlace", "true/false");
         gifInterlaceParam.setDescription("GIF-Parameter: Boolean value true/false indicating if interlacing should be used.");
         parameterList.add(gifInterlaceParam);
+        Parameter gifDitherParam = new Parameter("gif-dither", "true/false");
+        gifDitherParam.setDescription("GIF-Parameter: Boolean value true/false indicating if dither should be used.");
+        parameterList.add(gifDitherParam);
+        Parameter gifNumcolorsParam = new Parameter("gif-dither", "0-256");
+        gifNumcolorsParam.setDescription("GIF-Parameter: Integer value in the range from 0 to 256 indicating the number of colors to be used.");
+        parameterList.add(gifNumcolorsParam);
+        Parameter gifAlphaditherParam = new Parameter("gif-alphadither", "true/false");
+        gifAlphaditherParam.setDescription("GIF-Parameter: Boolean value true/false indicating if alpha dither should be used.");
+        parameterList.add(gifAlphaditherParam);
+        Parameter gifRemunusedParam = new Parameter("gif-removeunused", "true/false");
+        gifRemunusedParam.setDescription("GIF-Parameter: Boolean value true/false indicating if unused colors should be removed");
+        parameterList.add(gifRemunusedParam);
 
         // eps
         Parameter epsPostscriptLevel2Param = new Parameter("epsPostscriptLevel2", "true/false");
@@ -274,7 +427,7 @@ public final class Gimp26Migration implements Migrate, Serializable {
         FormatRegistryImpl fmtRegImpl = new FormatRegistryImpl();
         Format uriFormatObj = fmtRegImpl.getFormatForURI(formatUri);
         Set<String> reqInputFormatExts = uriFormatObj.getExtensions();
-        Iterator itrReq =reqInputFormatExts.iterator(); 
+        Iterator itrReq = reqInputFormatExts.iterator(); 
         // Iterate either over input formats ArrayList or over output formats
         // HasMap
         Iterator itrGimp = (isOutput)?outputFormats.keySet().iterator():inputFormats.iterator();
@@ -320,25 +473,23 @@ public final class Gimp26Migration implements Migrate, Serializable {
      * @return Null if the input or output format is not supported, fu-script 
      * command string otherwise
      */
-    private String getFuScriptMigrationStr( URI inputFormat, URI outputFormat )
+    private String getFuScriptMigrationStr()
     {
         // fu-script string of the planets migration command
         String fuScriptMigrString = null;
-        String inputFmtStr = getFormatExt( inputFormat, false );
-        String outputFmtStr = getFormatExt( outputFormat, true );
-        // 
-        if( inputFmtStr != null && outputFmtStr != null )
+        
+        if( inputFmtExt != null && outputFmtExt != null )
         {
             // build fu-script command of the form e.g.
             // planetsMigrateJPEGtoPNG(infile.ext outfile.ext 1 1 1)
             fuScriptMigrString = 
                             "(planetsMigrate" + 
-                            inputFmtStr +
+                            inputFmtExt +
                             "to" +
-                            outputFmtStr +
+                            outputFmtExt +
                             " \"" + tmpInFile.getAbsolutePath() + "\"" +
                             " \"" + tmpInFile.getAbsolutePath() + ".out\" " +
-                            outputFormats.get(outputFmtStr) + 
+                            outputFormats.get(outputFmtExt) + 
                             ")";
         }
         return fuScriptMigrString;
