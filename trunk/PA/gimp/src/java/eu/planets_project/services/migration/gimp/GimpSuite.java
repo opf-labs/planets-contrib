b22379fe4dee20c1418fe7279fe8b518b5ae2069
Georg Petz <georg.petz@onb.ac.at>
2008-12-10 10:11:53 +0000

    added TestSuite for Gimp and DviPs

---
+++ trunk/PA/gimp/src/java/eu/planets_project/services/migration/gimp/GimpSuite.java
@@ -0,0 +1,10 @@
+package eu.planets_project.services.migration.gimp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Gimp26Migration.class})
+public class GimpSuite {
+
+}
