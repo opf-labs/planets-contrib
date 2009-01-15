dfd3dcc0290426203ae36c8e0a2f057bb51ecfb7
Peter Melms <peter.melms@uni-koeln.de>
2009-01-15 16:43:15 +0000

    - Added new XcdlExtractor implementing mMgrate - added test cases for XcdlExtractor.java - added test resources (images and xcel files) for tests - modified CoreExtractor for correct Parameter handling - minor edit in Extractor to use the read() method in Content instead of getValue() - minor edit in SoxMigrations.java

--- trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
+++ trunk/PA/sox/src/java/eu/planets_project/services/migration/soxservices/SoxMigrations.java
@@ -1,7 +1,6 @@
 package eu.planets_project.services.migration.soxservices;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,6 +8,7 @@ import eu.planets_project.services.utils.ByteArrayHelper;
 import eu.planets_project.services.utils.FileUtils;
 import eu.planets_project.services.utils.PlanetsLogger;
 import eu.planets_project.services.utils.ProcessRunner;
+import eu.planets_project.services.datatypes.Parameters;
 
 /**
  * Common sox invocation class
@@ -59,14 +59,15 @@ public class SoxMigrations {
     				"and point it to the SoX installation folder!");
     	}
 		
-        plogger.info("Pointed to sox in: " + SOX_HOME);
-        System.out.println("Pointed SoX_HOME to: " + SOX_HOME);
+        plogger.info("Found SoX installation in: " + SOX_HOME);
+        System.out.println("Found SoX installation in: " + SOX_HOME);
     }
 
 	/**
 	 * @param input
 	 * @return the migrated OGG file
 	 */
+    @Deprecated
 	public byte[] transformMp3ToOgg(byte[] input) {
         plogger.info("transformMp3ToOgg begin ");
         return genericTransformAudioSrcToAudioDest(input, ".mp3", ".ogg", null);
@@ -76,6 +77,7 @@ public class SoxMigrations {
 	 * @param input
 	 * @return the migrated AIFF file
 	 */
+    @Deprecated
     public byte[] transformWavToAiff(byte[] input) {
     	plogger.info("transformWavToAiff begin ");
     	return genericTransformAudioSrcToAudioDest(input, ".wav", ".aiff", null);
@@ -85,6 +87,7 @@ public class SoxMigrations {
 	 * @param input
 	 * @return the migrated WAV file
 	 */
+    @Deprecated
     public byte[] transformMp3ToWav(byte[] input) {
         plogger.info("transformMp3ToWav begin ");
         return genericTransformAudioSrcToAudioDest(input, ".mp3", ".wav", null);
@@ -94,6 +97,7 @@ public class SoxMigrations {
 	 * @param input
 	 * @return the migrated OGG file
 	 */
+    @Deprecated
     public byte[] transformWavToOgg(byte[] input) {
         plogger.info("transformWavToOgg begin ");
         return genericTransformAudioSrcToAudioDest(input, ".wav", ".ogg", null);
@@ -103,6 +107,7 @@ public class SoxMigrations {
 	 * @param input
 	 * @return the migrated FLAC file
 	 */
+    @Deprecated
     public byte[] transformWavToFlac(byte[] input) {
         plogger.info("transformWavToFlac begin ");
         return genericTransformAudioSrcToAudioDest(input, ".wav", ".flac", null);
@@ -112,55 +117,22 @@ public class SoxMigrations {
 	 * @param input
 	 * @return the migrated FLAC file
 	 */
+    @Deprecated
     public byte[] transformMp3ToFlac(byte[] input) {
         plogger.info("transformMp3ToFlac begin ");
         return genericTransformAudioSrcToAudioDest(input, ".mp3", ".flac", null);
     }
 
-//    public DataHandler transformMp3ToOggDH(DataHandler input) {
-//        log.info("transformMp3ToOggDH begin ");
-//        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".ogg",
-//                null);
-//    }
-//    
-//    public DataHandler transformWavToAiffDH(DataHandler input) {
-//    	log.info("transformWavToRAW begin ");
-//    	return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".aiff", null);
-//    }
-//
-//    public DataHandler transformMp3ToWavDH(DataHandler input) {
-//        log.info("transformMp3ToWavDH begin ");
-//        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".wav",
-//                null);
-//    }
-//
-//    public DataHandler transformWavToOggDH(DataHandler input) {
-//        log.info("transformWavToOggDH begin ");
-//        return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".ogg",
-//                null);
-//    }
-//
-//    public DataHandler transformWavToFlacDH(DataHandler input) {
-//        log.info("transformWavToFlacDH begin ");
-//        return genericTransformAudioSrcToAudioDestDH(input, ".wav", ".flac",
-//                null);
-//    }
-//
-//    public DataHandler transformMp3ToFlacDH(DataHandler input) {
-//        log.info("transformMp3ToFlacDH begin ");
-//        return genericTransformAudioSrcToAudioDestDH(input, ".mp3", ".flac",
-//                null);
-//    }
 
     /**
      * @param input
      * @param srcSuffix
      * @param destSuffix
-     * @param soxCliParams
+     * @param parameters
      * @return the migrated byte[]
      */
     public byte[] genericTransformAudioSrcToAudioDest(byte[] input,
-            String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
+            String srcSuffix, String destSuffix, Parameters parameters) {
     	
         if (!srcSuffix.startsWith("."))
             srcSuffix = "." + srcSuffix;
@@ -185,8 +157,8 @@ public class SoxMigrations {
         try {
             List<String> commands = Arrays.asList(SOX_HOME + SOX, inputFile
                     .getAbsolutePath(), outputFilePath);
-            if(soxCliParams!=null) {
-            	commands.addAll(soxCliParams);
+            if(parameters!=null) {
+            	// TO DO: fill in parameter handling!
             }
             
             ProcessRunner pr = new ProcessRunner(commands);
@@ -222,53 +194,4 @@ public class SoxMigrations {
         
         return outputFileData;
     }
-
-    
-    
-//    public DataHandler genericTransformAudioSrcToAudioDestDH(DataHandler input,
-//            String srcSuffix, String destSuffix, ArrayList<String> soxCliParams) {
-//        log
-//                .info("genericTransformAudioSrcToAudioDestDH begin : Converting from "
-//                        + srcSuffix + " to " + destSuffix);
-//        File f = FileUtils.tempFile("tempout.audio", destSuffix);
-//        byte[] raw = null;
-//        try {
-//            FileOutputStream fos = new FileOutputStream(f);
-//            log.info(input.getContentType());
-//            log.info(raw);
-//            log.info(input.getContent().toString());
-//            log.info(raw);
-//            fos.write(raw);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        File inputFile = FileUtils.tempFile(raw, "tempin.audio", srcSuffix);
-////        if (!f.canRead() || !inputFile.exists() || !f.exists() || !f.canWrite()
-////                || !inputFile.canRead() || !inputFile.canWrite()) {
-////            throw new IllegalStateException("Can't read from or write to: "
-////                    + f.getAbsolutePath());
-////        }
-//        try {
-//            List<String> commands = Arrays.asList(SOX_HOME + File.separator + SOX, inputFile
-//                    .getAbsolutePath(), f.getAbsolutePath());
-//            File home = new File(SOX_HOME);
-//            if (soxCliParams != null) {
-//                commands.addAll(soxCliParams);
-//            }
-//            ProcessRunner pr = new ProcessRunner(commands);
-//            pr.setStartingDir(home);
-//            log.info("Executing: " + commands);
-//            pr.run();
-//            log.info("SOX call output: " + pr.getProcessOutputAsString());
-//            log.error("SOX call error: " + pr.getProcessErrorAsString());
-//            log.debug("Executing: " + commands + " finished.");
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//            log.error("SoX could not create the open document file");
-//        }
-//        log.info("genericTransformAudioSrcToAudioDestDH end");
-//        log.info(f.length());
-//        return new DataHandler(new ByteArrayDataSource(ByteArrayHelper.read(f),
-//                "application/octet-stream"));
-//    }
 }
