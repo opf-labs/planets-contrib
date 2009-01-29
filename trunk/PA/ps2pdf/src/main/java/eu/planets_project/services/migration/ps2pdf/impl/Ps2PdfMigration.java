7f3b627d155e05f06843b5f05d1de4b7e42ae9fd
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2009-01-29 13:30:43 +0000

    Updated ps2pdf service to the new interface

--- trunk/PA/ps2pdf/src/main/java/eu/planets_project/services/migration/ps2pdf/impl/Ps2PdfMigration.java
+++ trunk/PA/ps2pdf/src/main/java/eu/planets_project/services/migration/ps2pdf/impl/Ps2PdfMigration.java
@@ -38,7 +38,7 @@ import java.util.ArrayList;
 @Local(Migrate.class)
 @Remote(Migrate.class)
 @Stateless
-@WebService(/* name = Pdf2PsMigration.NAME ,*/
+@WebService( name = Ps2PdfMigration.NAME ,
         serviceName = Migrate.NAME,
         targetNamespace = PlanetsServices.NS,
         endpointInterface = "eu.planets_project.services.migrate.Migrate" )
@@ -47,24 +47,10 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
     PlanetsLogger log = PlanetsLogger.getLogger(Ps2PdfMigration.class);
 
 
-
-
-    /** The pdf2ps installation dir */
-    public String pdf2ps_install_dir;
-    /** The pdf2ps application name */
-    public String pdf2ps_app_name;
-    /** The output file extension */
-    public String pdf2ps_outfile_ext;
-    private File tmpInFile;
-    private File tmpOutFile;
+    private String ps2pdf_app_name;
 
     /** The service name */
     public static final String NAME = "Ps2PdfBasicMigration";
-
-    /***/
-    private static final long serialVersionUID = 1878137433497934255L;
-
-
 
 
     /**
@@ -81,19 +67,17 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
             String strRsc = "/eu/planets_project/services/migration/ps2pdf/ps2pdf.properties";
             props.load( this.getClass().getResourceAsStream(strRsc));
             // config vars
-            this.pdf2ps_install_dir = props.getProperty("pdf2ps.install.dir");
-            this.pdf2ps_app_name = props.getProperty("pdf2ps.app.name");
-            this.pdf2ps_outfile_ext = props.getProperty("pdf2ps.outfile.ext");
+
+
+            this.ps2pdf_app_name = props.getProperty("pdf2ps.app.name");
+
 
         } catch( Exception e ) {
             // // config vars
-            this.pdf2ps_install_dir  = "/usr/bin/";
-            this.pdf2ps_app_name = "pdf2ps";
-            this.pdf2ps_outfile_ext = "ps";
+            this.ps2pdf_app_name = "ps2pdf";
         }
-        log.info("Using pdf2ps install directory: "+this.pdf2ps_install_dir);
-        log.info("Using pdf2ps application name: "+this.pdf2ps_app_name);
-        log.info("Using pdf2ps outfile extension: "+this.pdf2ps_outfile_ext);
+
+        log.info("Using ps2pdf application name: "+this.ps2pdf_app_name);
 
         /*
          * We just return a new digital object with the same required arguments
@@ -104,7 +88,7 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
 
         ProcessRunner runner = new ProcessRunner();
         List<String> command = new ArrayList<String>();
-        command.add("ps2pdf");
+        command.add(ps2pdf_app_name);
         command.add("-");
         command.add("-");
 
@@ -115,31 +99,24 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
 
 
 
-
-
         runner.run();
         int return_code = runner.getReturnCode();
 
+        ServiceReport report = new ServiceReport();
+
         if (return_code != 0){
-
-
-            throw new RuntimeException("Execution failed:" + runner.getProcessErrorAsString());
+            report.setErrorState(return_code);
+            report.setError(runner.getProcessOutputAsString()+"\n"+runner.getProcessErrorAsString());
+            return new MigrateResult(null,report);
         }
         InputStream pdfFileStream = runner.getProcessOutput();
         byte[] pdfbytes = FileUtils.writeInputStreamToBinary(pdfFileStream);
 
         DigitalObject pdfFile = new DigitalObject.Builder(Content.byValue(pdfbytes)).build();
-
-        ServiceReport report = new ServiceReport();
         return new MigrateResult(pdfFile,report);
-
-
 
     }
 
-    @WebMethod(operationName = Migrate.NAME + "_" + "describe", action = PlanetsServices.NS + "/" + Migrate.NAME + "/" + "describe")
-    @WebResult(name = Migrate.NAME + "Description", targetNamespace = PlanetsServices.NS + "/" + Migrate.NAME, partName = Migrate.NAME + "Description")
-    @ResponseWrapper(className = "eu.planets_project.services.migrate." + Migrate.NAME + "DescribeResponse")
     public ServiceDescription describe() {
         ServiceDescription.Builder builder = new ServiceDescription.Builder(NAME, Migrate.class.getName());
 
