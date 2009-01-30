5cb25da11313a76173a000057683e2f6737c5557
shsdev <shsdev@unknown.com>
2009-01-30 15:32:43 +0000

    Minor changes

--- trunk/PA/jasper19/test/eu/planets_project/services/migration/jasper/Jasper19Suite.java
+++ trunk/PA/jasper19/test/eu/planets_project/services/migration/jasper/Jasper19Suite.java
@@ -1,5 +1,6 @@
 package eu.planets_project.services.migration.jasper;
 
+import eu.planets_project.services.migration.jasper.Jasper19MigrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
