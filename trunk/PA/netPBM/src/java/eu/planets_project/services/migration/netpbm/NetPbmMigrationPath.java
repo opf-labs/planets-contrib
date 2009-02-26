73eb23cfd4dbb95d6602400187feff4a6b0bb26b
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2009-02-26 14:51:03 +0000

    netpbm with some formats and migrationpaths

--- trunk/PA/netPBM/src/java/eu/planets_project/services/migration/netpbm/NetPbmMigrationPath.java
+++ trunk/PA/netPBM/src/java/eu/planets_project/services/migration/netpbm/NetPbmMigrationPath.java
@@ -4,7 +4,18 @@ package eu.planets_project.services.migration.netpbm;
  * TODO abr forgot to document this class
  */
 public enum NetPbmMigrationPath {
+    pnmtopnm(NetPbmFormat.pnm,NetPbmFormat.pnm,new String[]{"pnmtopnm"}),
+    pgmtopgm(NetPbmFormat.pgm,NetPbmFormat.pgm,new String[]{"pgmtopgm"}),
+    ppmtoppm(NetPbmFormat.ppm,NetPbmFormat.ppm,new String[]{"ppmtoppm"}),
+    pamtopam(NetPbmFormat.pam,NetPbmFormat.pam,new String[]{"pamtopam"}),
+    pbmtopgm(NetPbmFormat.pbm,NetPbmFormat.pgm,new String[]{"pbmtopgm"}),
+    pgmtopbm(NetPbmFormat.pgm,NetPbmFormat.pbm,new String[]{"pgmtopbm"}),
+    pgmtoppm(NetPbmFormat.pgm,NetPbmFormat.ppm,new String[]{"pgmtopbm"}),
+    ppmtopgm(NetPbmFormat.pbm,NetPbmFormat.pgm,new String[]{"ppmtopgm"}),
+
+    jpegtopng(NetPbmFormat.jpeg,NetPbmFormat.png,new String[]{"jpegtopnm","|","pnmtopng"}),
     ;
+
 
     private NetPbmFormat in;
     private NetPbmFormat out;
