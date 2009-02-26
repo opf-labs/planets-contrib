f2edc45c5a74688c53843b505912b665f425dcee
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2009-02-26 14:15:34 +0000

    FFMpeg wrapped. All program code hopefully written. Make the tool functional by populating the Enums

---
+++ trunk/PA/ffmpeg/src/java/eu/planets_project/services/migration/ffmpeg/FFMpegMigration.java
@@ -0,0 +1,166 @@
+package eu.planets_project.services.migration.ffmpeg;
+
+import eu.planets_project.services.PlanetsServices;
+import eu.planets_project.services.datatypes.*;
+import eu.planets_project.services.migrate.Migrate;
+import eu.planets_project.services.migrate.MigrateResult;
+import eu.planets_project.services.utils.FileUtils;
+import eu.planets_project.services.utils.PlanetsLogger;
+import eu.planets_project.services.utils.ProcessRunner;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.xml.ws.BindingType;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Pdf2HtmlMigration migrates between its own formats and a number of formats
+ *
+ * @author Asger Blekinge-Rasmussen <abr@statsbiblioteket.dk>
+ */
+@Local(Migrate.class)
+@Remote(Migrate.class)
+@Stateless
+@BindingType(value = "http://schemas.xmlsoap.org/wsdl/soap/http?mtom=true")
+@WebService(
+        name = FFMpegMigration.NAME,
+        serviceName = Migrate.NAME,
+        targetNamespace = PlanetsServices.NS,
+        endpointInterface = "eu.planets_project.services.migrate.Migrate")
+public class FFMpegMigration implements Migrate, Serializable {
+
+    PlanetsLogger log = PlanetsLogger.getLogger(FFMpegMigration.class);
+
+    /**
+     * The service name.
+     */
+    static final String NAME = "FFMpegMigration";
+
+    /**
+     * {@inheritDoc}
+     *
+     * @see eu.planets_project.services.migrate.Migrate#migrate(eu.planets_project.services.datatypes.DigitalObject, java.net.URI, java.net.URI, eu.planets_project.services.datatypes.Parameters)
+     */
+    public MigrateResult migrate(final DigitalObject digitalObject,
+                                 URI inputFormat, URI outputFormat, Parameters parameters) {
+
+        java.util.Properties props = new java.util.Properties();
+        try {
+
+            String strRsc = "/eu/planets_project/services/migration/ffmpeg/ffmpeg.properties";
+            props.load( this.getClass().getResourceAsStream(strRsc));
+            // config vars
+
+
+        } catch( Exception e ) {
+            // // config vars
+
+        }
+
+        ServiceReport report = new ServiceReport();
+        //log.info("Using ps2pdf application name: "+this.ps2pdf_app_name);
+
+
+        /*
+         * We just return a new digital object with the same required arguments
+         * as the given:
+         */
+
+        FFMpegFormat in = null;
+        try {
+            in = FFMpegFormat.loopup(inputFormat);
+        } catch (NoSuchFormatException e) {
+            report.setError("The input format is not on the known list");
+            return fail(report);
+        }
+        FFMpegFormat out = null;
+        try {
+            out = FFMpegFormat.loopup(outputFormat);
+        } catch (NoSuchFormatException e) {
+            report.setError("The output format is not on the known list");
+            return fail(report);
+        }
+        String[] migrationPath = null;
+
+        try {
+            migrationPath = FFMpegMigrationPath.lookup(in,out).getTool();
+        } catch (NoSuchMigrationPathException e) {
+            report.setError("No conversionpath for these formats");
+            return fail(report);
+
+        }
+
+
+        InputStream psfile = digitalObject.getContent().read();
+
+
+
+        ProcessRunner runner = new ProcessRunner();
+        List<String> command = new ArrayList<String>();
+        for (String element : migrationPath){
+            command.add(element);
+        }
+
+
+        runner.setCommand(command);
+        runner.setInputStream(psfile);
+        runner.setCollection(true);
+        runner.setOutputCollectionByteSize(-1);
+
+
+
+        runner.run();
+        int return_code = runner.getReturnCode();
+
+
+
+        if (return_code != 0){
+            report.setErrorState(return_code);
+            report.setError(runner.getProcessOutputAsString()+"\n"+runner.getProcessErrorAsString());
+            return fail(report);
+        }
+        InputStream newFileStream = runner.getProcessOutput();
+        byte[] outbytes = FileUtils.writeInputStreamToBinary(newFileStream);
+
+        DigitalObject pdfFile = new DigitalObject.Builder(Content.byValue(outbytes)).build();
+        return new MigrateResult(pdfFile,report);
+
+    }
+
+    /**
+     * @see eu.planets_project.services.migrate.Migrate#describe()
+     * @return ServiceDescription
+     */
+    public ServiceDescription describe() {
+
+        ServiceDescription.Builder builder = new ServiceDescription.Builder(NAME, Migrate.class.getName());
+
+        builder.author("Asger Blekinge-Rasmussen <abr@statsbiblioteket.dk>");
+        builder.classname(this.getClass().getCanonicalName());
+        builder.description("Converts between a number of image formats");
+
+        FFMpegMigrationPath[] mypaths = FFMpegMigrationPath.values();
+        MigrationPath[] paths = new MigrationPath[mypaths.length];
+        int i=0;
+        for (FFMpegMigrationPath mypath: mypaths){
+            paths[i] = new MigrationPath(mypath.getIn().getFormat(),mypath.getOut().getFormat(),null);
+        }
+
+        builder.paths(paths);
+        builder.version("0.1");
+
+        return builder.build();
+
+    }
+
+
+    private MigrateResult fail(ServiceReport report){
+        return new MigrateResult(null,report);
+    }
+}
---
+++ trunk/PA/ffmpeg/src/java/eu/planets_project/services/migration/ffmpeg/NoSuchFormatException.java
@@ -0,0 +1,7 @@
+package eu.planets_project.services.migration.ffmpeg;
+
+/**
+ * TODO abr forgot to document this class
+ */
+public class NoSuchFormatException extends Exception {
+}
