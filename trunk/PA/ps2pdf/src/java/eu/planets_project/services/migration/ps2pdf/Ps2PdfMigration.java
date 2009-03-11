25d5f1064c6d5f25a71f84c414357f93df783bf6
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2009-03-11 16:07:14 +0000

    REfactor of ps2pdf

--- trunk/PA/ps2pdf/src/java/eu/planets_project/services/migration/ps2pdf/Ps2PdfMigration.java
+++ trunk/PA/ps2pdf/src/java/eu/planets_project/services/migration/ps2pdf/Ps2PdfMigration.java
@@ -12,16 +12,17 @@ import eu.planets_project.services.migrate.MigrateResult;
 import eu.planets_project.services.utils.FileUtils;
 import eu.planets_project.services.utils.PlanetsLogger;
 import eu.planets_project.services.utils.ProcessRunner;
-import eu.planets_project.services.utils.ServiceUtils;
 
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.jws.WebService;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.*;
 import java.util.Properties;
 
@@ -48,6 +49,36 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
     /** The service name */
     public static final String NAME = "Ps2PdfBasicMigration";
 
+    private  Set<URI> inputformats;
+    private Set<URI> outputformats;
+
+
+    public Ps2PdfMigration() throws URISyntaxException, IOException {
+
+        inputformats = new HashSet<URI>();
+        outputformats = new HashSet<URI>();
+        Properties props = new Properties();
+
+
+        String strRsc = "ps2pdf.properties";
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL resourceURL = loader.getResource(strRsc);
+
+        props.load(resourceURL.openStream());
+        // config vars
+
+        this.ps2pdf_app_name = props.getProperty("ps2pdf.app.name");
+        String[] iformats = props.getProperty("ps2pdf.inputformats").split(",");
+        for (String format: iformats){
+            inputformats.add(new URI(format.trim()));
+        }
+
+        String[] oformats = props.getProperty("ps2pdf.outputformats").split(",");
+        for (String format: oformats){
+            outputformats.add(new URI(format.trim()));
+        }
+    }
+
 
     /**
      * {@inheritDoc}
@@ -57,28 +88,23 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
     public MigrateResult migrate( final DigitalObject digitalObject, URI inputFormat,
                                   URI outputFormat, Parameters parameters) {
 
-        Properties props = new Properties();
-        try {
 
-            String strRsc = "/eu/planets_project/services/migration/ps2pdf/ps2pdf.properties";
-            props.load( this.getClass().getResourceAsStream(strRsc));
-            // config vars
+        ServiceReport report = new ServiceReport();
 
-
-            this.ps2pdf_app_name = props.getProperty("pdf2ps.app.name");
-
-
-        } catch( Exception e ) {
-            // // config vars
-            this.ps2pdf_app_name = "ps2pdf";
+        if (inputFormat != null && !inputformats.contains(inputFormat)){
+            report.setError("Unknown inputformat set, use another tool that accepts this format");
+            return new MigrateResult(null,report);
         }
 
-        log.info("Using ps2pdf application name: "+this.ps2pdf_app_name);
+        if (outputFormat == null || !outputformats.contains(outputFormat)){
+            report.setError("Unknown outputformat set, use another tool that accepts this format");
+            return new MigrateResult(null,report);
+        }
 
         /*
-         * We just return a new digital object with the same required arguments
-         * as the given:
-         */
+        * We just return a new digital object with the same required arguments
+        * as the given:
+        */
 
         InputStream psfile = digitalObject.getContent().read();
 
@@ -93,22 +119,27 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
         runner.setCollection(true);
         runner.setOutputCollectionByteSize(-1);
 
-
-
         runner.run();
         int return_code = runner.getReturnCode();
-
-        ServiceReport report = new ServiceReport();
 
         if (return_code != 0){
             report.setErrorState(return_code);
             report.setError(runner.getProcessOutputAsString()+"\n"+runner.getProcessErrorAsString());
             return new MigrateResult(null,report);
         }
+
+
         InputStream pdfFileStream = runner.getProcessOutput();
         byte[] pdfbytes = FileUtils.writeInputStreamToBinary(pdfFileStream);
 
-        DigitalObject pdfFile = new DigitalObject.Builder(Content.byValue(pdfbytes)).build();
+        DigitalObject pdfFile = new DigitalObject.
+                Builder(Content.byValue(pdfbytes))
+                .format(outputFormat)
+                .manifestationOf(digitalObject.getManifestationOf())
+                .metadata((Metadata[]) digitalObject.getMetadata().toArray())
+                .title(digitalObject.getTitle())
+                .build();
+
         return new MigrateResult(pdfFile,report);
 
     }
@@ -121,19 +152,13 @@ public final class Ps2PdfMigration implements Migrate, Serializable {
         builder.description("Simple ps2pdf wrapper for Ps (postscript) to PDF conversions.");
         FormatRegistry fm= FormatRegistryFactory.getFormatRegistry();
 
+        MigrationPath[] paths = MigrationPath.constructPaths(inputformats,outputformats);
+        builder.paths(paths);
+        builder.version("0.1");
 
+        ServiceDescription mds =builder.build();
 
-        try {
-            MigrationPath[] paths = MigrationPath.constructPaths(fm.getURIsForExtension("ps"), ServiceUtils.asSet(new URI("info:pronom/fmt/16")));
-            builder.paths(paths);
-            builder.version("0.1");
-
-            ServiceDescription mds =builder.build();
-
-            return mds;
-        } catch (URISyntaxException e) {
-            throw new Error("Hardcoded uri invalid, should not happen",e);
-        }
+        return mds;
 
 
     }
