8e2d8766b0e79897856778b11edc8d32d9b3b46b
Andrew Jackson <Andrew.Jackson@bl.uk>
2009-06-04 13:25:20 +0000

    Need a suite to run tests from.

---
+++ trunk/PA/mdb2siard/test/eu/planets_project/services/migration/mdb2siard/AllSiardTestsuite.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2007, 2008 The Planets Project Partners.
+ * 
+ * All rights reserved. This program and the accompanying 
+ * materials are made available under the terms of the 
+ * GNU Lesser General Public License v3 which 
+ * accompanies this distribution, and is available at 
+ * http://www.gnu.org/licenses/lgpl.html
+ * 
+ */
+package eu.planets_project.services.migration.mdb2siard;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author AnJackson
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {Mdb2SiardMigrateTester.class })
+public class AllSiardTestsuite {}
