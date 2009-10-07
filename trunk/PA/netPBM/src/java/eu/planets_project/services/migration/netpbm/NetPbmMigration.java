8971324ed31dab0d0ebd618c584fe077443eab4b
Thomas Skou Hansen <tsh@statsbiblioteket.dk>
2009-10-07 12:10:12 +0000

    Renamed the generic wrapper package to allow usage of different versions simultaneously in the same release of the system.

--- trunk/PA/netPBM/src/java/eu/planets_project/services/migration/netpbm/NetPbmMigration.java
+++ trunk/PA/netPBM/src/java/eu/planets_project/services/migration/netpbm/NetPbmMigration.java
@@ -23,9 +23,9 @@ import eu.planets_project.services.datatypes.ServiceReport.Type;
 import eu.planets_project.services.migrate.Migrate;
 import eu.planets_project.services.migrate.MigrateResult;
 import eu.planets_project.services.utils.PlanetsLogger;
-import eu.planets_project.ifr.core.services.migration.genericwrapper.utils.DocumentLocator;
-import eu.planets_project.ifr.core.services.migration.genericwrapper.GenericMigrationWrapper;
-import eu.planets_project.ifr.core.services.migration.genericwrapper.exceptions.MigrationInitialisationException;
+import eu.planets_project.ifr.core.services.migration.genericwrapper1.GenericMigrationWrapper;
+import eu.planets_project.ifr.core.services.migration.genericwrapper1.exceptions.MigrationInitialisationException;
+import eu.planets_project.ifr.core.services.migration.genericwrapper1.utils.DocumentLocator;
 
 /**
  * The class migrates between a number of formats
